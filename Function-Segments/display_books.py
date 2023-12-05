f25df378ad02aef1b4d127f02c77fbb30d7a0ae2
4dummy <149559666+4dummy@users.noreply.github.com>
2023-12-05 17:40:12 +0800

    Added columns to display function

--- Function-Segments/display_books.py
+++ Function-Segments/display_books.py
@@ -1,13 +1,13 @@
 from get_books import get_books
 
-# ! DARRANCE !
+# ! ASK DARRANCE LATER !
 # in the assignment details, it says:
 #
 # Data from books_StudentID.txt must be read and stored in the program when the program first runs
 # so that all the book information previously stored in the text file will be displayed
 # if it is the first option selected when starting the program.
 #
-# so I'm not sure if we need to do some kind of open() function at the start of the program?
+# do we need to do some kind of open() function at the start of the program?
 
 
 def display_books():  # add headings for columns!!!
@@ -22,13 +22,31 @@ def display_books():  # add headings for columns!!!
 ██╔══██║██║░░░░░██║░░░░░  ██╔══██╗██║░░██║██║░░██║██╔═██╗░░╚═══██╗
 ██║░░██║███████╗███████╗  ██████╦╝╚█████╔╝╚█████╔╝██║░╚██╗██████╔╝
 ╚═╝░░╚═╝╚══════╝╚══════╝  ╚═════╝░░╚════╝░░╚════╝░╚═╝░░╚═╝╚═════╝░''')
+    print("\n")
+
+    categories = ["ISBN", "AUTHOR", "TITLE", "PUBLISHER",
+                  "GENRE", "YEAR PUBLISHED", "DATE PURCHASED", "STATUS"]
+
+    for category in categories:
+
+        column_length = get_max_column_length(categories.index(category)) + 3
+        # +3 to leave a little space before the next column and prevent them from being too close together.
+
+        if column_length < len(category):
+            column_length = len(category) + 3
+
+        print(f"{category: <{column_length}}", end="")
+
+    print("\n")
 
     '''
     The book list is split into individual books.
     The individual books are further split into their details.
 
     The longest column length (the length of the longest detail in the column) is determined.
-    Then, the detail is printed in a space that is the longest column length + 2 characters.
+    If the length of the name of the category is longer than the longest column length, the column is widened to its length.
+
+    Then, the detail is printed in a consistently-sized space.
     This is done for every detail of the book.
 
     A newline is created to separate each book, and the process repeats for each book.
@@ -37,12 +55,17 @@ def display_books():  # add headings for columns!!!
     book_list = get_books()
 
     for book in book_list:
+
         book_details = book.split("|")
 
         for detail in book_details:
+
             column_length = get_max_column_length(
-                book_details.index(detail)) + 2
-            # +2 to leave a little space before the next column and prevent them from being too close together.
+                book_details.index(detail)) + 3
+            # +3 to leave a little space before the next column and prevent them from being too close together.
+
+            if column_length < len(categories[book_details.index(detail)]):
+                column_length = len(categories[book_details.index(detail)]) + 3
 
             print(f"{detail: <{column_length}}", end="")
 
@@ -74,6 +97,7 @@ def get_max_column_length(column):
     book_list = get_books()
 
     for book in book_list:
+
         book_details = book.split("|")
 
         column_lengths.append(len(book_details[column]))
