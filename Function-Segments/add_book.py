09a7d79764591e8417e1ac393ceac5a0ec80e8d8
LeeIsaac2004 <152567159+LeeIsaac2004@users.noreply.github.com>
2023-12-06 22:30:17 +0800

    Update add_book.py
    Revision 2

--- Function-Segments/add_book.py
+++ Function-Segments/add_book.py
@@ -1,39 +1,40 @@
+import traceback
 from datetime import date
 
-'''
-These lines of codes allows users to add books by inputting various details about the book, such as its ISBN number, author, title, publisher, genre, published year, date purchased and the current status of the book.
-'''
-def add_book():
-    with open("books_23094907.txt", 'a') as f:
-        print("Please enter the following information:")
-        while True:
-            isbn = input("ISBN number: ")
-            if len(isbn) == 13:
-                break
-            else:
-                print("Error. ISBN number must be 13 digits long. Please try again.")
-        author = input("Author's name: ")
-        title = input("Book's title: ")
-        publisher = input("Publisher's name: ")
-        genre = input("Genre of the book: ")
-        published_year = input("Publishing year: ")
-        date_purchased_str = input("Date purchased in the format DD-MM-YYYY: ")
-        date_purchased = date.strptime(date_purchased_str, '%d-%m-%Y')
-        status = input("Book's status: ")
+    '''
+    The Book class is used to represent a book.
+    '''
+class Book:
+    def __init__(self, isbn, author, title, publisher, genre, published_year, date_purchased, status):
+        self.isbn = isbn
+        self.author = author
+        self.title = title
+        self.publisher = publisher
+        self.genre = genre
+        self.published_year = published_year
+        self.date_purchased = date_purchased
+        self.status = status
 
-        '''
-        This line of code formats the book informations into a specific format.
-        '''
-        book_information = f"{isbn}\t\t{author}\t\t{title}\t\t{publisher}\t\t{status}"
-        
-        '''
-        This line of code writes the book information to the books_23094907.txt file.
-        '''
-        f.write(book_information + '\n')
+    '''
+    The add_book() function is used to add a book to the books_23094907.txt file.
+    '''
+def add_book(new_book):
+    try:
+        with open("books_23094907.txt", 'a') as f:
+            book_information = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+                new_book.isbn, new_book.author, new_book.title, new_book.publisher, new_book.genre, new_book.published_year, new_book.date_purchased, new_book.status)
+            f.write(book_information + '\n')
+        print("Book added successfully!")
+    except Exception as e:
+        print("Error occurred: ", e, "\n", traceback.format_exc())
 
-    print("Book added successfully!")
+new_book = Book(isbn="1234567890123",
+                author="Author's Name",
+                title="Book's Title",
+                publisher="Publisher's Name",
+                genre="Genre of the Book",
+                published_year="2023",
+                date_purchased=date.strptime("10-10-2023", '%d-%m-%Y'),
+                status="Book's Status")
 
-'''
-This line of code calls the function.
-'''
-add_book()
+add_book(new_book)
