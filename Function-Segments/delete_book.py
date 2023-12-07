ccee267de41d679341edb2db98cd254a8f339380
darrancebeh <darrancebeh@gmail.com>
2023-12-07 21:43:49 +0800

    Complete delete_book

--- Function-Segments/delete_book.py
+++ Function-Segments/delete_book.py
@@ -1,49 +1,41 @@
-
 from get_books import get_books
+from input_to_isbn import input_to_isbn
 
-def delete_book():
-    '''
-    Deletes books from the book list based on user input.
-    This function utilizes a previously defined function get_books to retrieve the
-    list of books from the database. It then prompts the user to enter information
-    about the book they want to delete. The function removes books from the list
-    containing the specified information and updates the original list.
-    '''
+
+def delete_book_interface():
+
     book_list = get_books()
 
-    while True:
-        # Display all books
-        print("Current Book List:")
-        for book in book_list:
-            print(book)
-
-        # Prompt user for input of isbn, author, or title for the book they want to delete
-        search_book = input("Enter ISBN, author, or title of the book you want to delete: ")
-
-        # Error handling for isbn/author/title input
-        if not search_book:
-            print("Invalid input. Please enter ISBN, author, or title.")
-            continue
-
-        # Use list comprehension to create a new list without the matching items
-        filtered_book_list = [book for book in book_list if search_book.lower() not in book.lower()]
-
-        if filtered_book_list != book_list:
-            print(f"The information '{search_book}' was present in the following books and has been removed:")
-            for item in book_list:
-                if item not in filtered_book_list:
-                    print(item)
-
-            # Update the original list with the filtered list
-            book_list = filtered_book_list
-        else:
-            print(f"The information '{search_book}' is not present in any of the books.")
-
-        # Ask the user whether they want to delete another book
-        another_deletion = input("Do you want to delete another book? (yes/no): ").lower()
-        if another_deletion != 'yes':
-            break
-
-    print("Updated Book List:")
     for book in book_list:
         print(book)
+
+    user_input_id = input_to_isbn()
+
+    if (user_input_id == 1):
+        delete_book_interface()
+    elif (user_input_id == 0):
+        print("Main Menu Function haha")
+        exit()
+
+    delete_book(user_input_id)
+
+
+def delete_book(isbn):
+
+    book_list = get_books()
+
+    for book in book_list:
+        book = book.split("|")
+        if isbn in book[0]:
+            book_list.remove("|".join(book))
+            break
+
+    with open("books_23094907.txt", "w") as f:
+        for book in book_list:
+            f.write(book)
+            f.write("\n")
+
+    print("Book Deleted Successfully!")
+
+
+delete_book_interface()
