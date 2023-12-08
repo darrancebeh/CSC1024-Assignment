bee0ed11baf9af3d2a3bede6b0f3924225d61fc4
LeeIsaac2004 <152567159+LeeIsaac2004@users.noreply.github.com>
2023-12-08 17:08:52 +0800

    Update add_book.py
    Sixth revision. Please inform me if there are any issues.

--- Function-Segments/add_book.py
+++ Function-Segments/add_book.py
@@ -4,18 +4,38 @@
 '''
 from datetime import datetime
 
+
 '''
-- The 'get_book_information()' function collects and validates information about a book from the user.
-- It prompts the user to enter the ISBN number, author's name, book's title, publisher's name, genre, publishing year, purchase date, and book's status.
-- Naming convention taken into consideration as people from different cultures have different naming conventions, such as umlauts and ligatures.
-- Also performs validation checks for the ISBN number to detect for existing matching books.
+- The 'def check_isbn(isbn)' is responsible for checking existing ISBNs. 
 '''
+
+
+def check_isbn(isbn):
+    with open("books_23094907.txt", 'r') as f:
+        for line in f:
+            if line.split('|')[0] == isbn:
+                return True
+    return False
+
+
+'''
+- The 'add_book_information()' function collects and validates information about a book from the user.
+- It prompts the user to enter the ISBN, author's name, book's title, publisher's name, genre, publishing year, purchase date, and book's status.
+- The naming convention is taken into consideration as people from different cultures have different naming conventions, such as umlauts and ligatures.
+- Also performs validation checks for the ISBN to detect existing matching books.
+'''
+
+
 def add_book_information():
     print("Please enter the following information:")
     while True:
         isbn = input("ISBN number: ")
         if len(isbn) == 13 and isbn.isdigit():
-            break
+            if check_isbn(isbn):
+                print("Error. A book with this ISBN already exists.")
+                continue
+            else:
+                break
         else:
             print(
                 "Error. ISBN number must be 13 digits long and contain only digits. Please try again.")
@@ -38,34 +58,46 @@ def add_book_information():
     while True:
         published_year = input("Publishing year: ")
         if len(published_year) == 4 and published_year.isdigit() and int(published_year) > 0 and int(published_year) <= datetime.now().year:
+            published_date = datetime(int(published_year), 1, 1)
             break
         else:
-            print(
-                "Error. Publishing year must be a 4-digit positive integer and not in the future. Please try again.")
+            print("Error. Publishing year must be a 4-digit positive integer and not in the future. Please try again.")
     while True:
         date_purchased_str = input("Date purchased in the format DD-MM-YYYY: ")
         try:
             date_purchased = datetime.strptime(date_purchased_str, '%d-%m-%Y')
-            if date_purchased <= datetime.now():
+            if date_purchased <= datetime.now() and date_purchased > published_date:
                 break
             else:
                 print(
-                    "Error. The purchase date cannot be in the future. Please try again.")
+                    "Error. The purchase date cannot be in the future and must be after the publishing year. Please try again.")
         except ValueError:
             print("Error. The date format is incorrect. Please try again.")
     while True:
-        print("Book's status options: Wishlist, To-read, Reading, Completed")
-        status = input("Book's status: ")
-        if status in ['Wishlist', 'To-read', 'Reading', 'Completed']:
+        print("Please input the book's status:")
+        print("[1] - Wishlist")
+        print("[2] - To-read")
+        print("[3] - Reading")
+        print("[4] - Completed")
+        print()
+        status_input = input("Book's status (enter a number from 1 to 4): ")
+        status_options = {'1': 'Wishlist', '2': 'To-read',
+                          '3': 'Reading', '4': 'Completed'}
+        if status_input in status_options:
+            status = status_options[status_input]
             break
         else:
             print("Error. Invalid status. Please try again.")
 
     return isbn, author, title, publisher, genre, published_year, date_purchased_str, status
 
+
 '''
-- Opens books_23094907.txt and write the new book's information into it.
+- The 'def add_book(book)' function inserts the accepted details into books_23094907.txt in a specific format.
+- Print out "Book added successfully!" if it's successful.
 '''
+
+
 def add_book(book):
     with open("books_23094907.txt", 'a') as f:
         isbn, author, title, publisher, genre, published_year, date_purchased_str, status = book
@@ -73,9 +105,12 @@ def add_book(book):
         f.write('\n' + book_information)
     print("Book added successfully!")
 
+
 '''
 - Calls the 'add_book_information()' function to collect book information from the user.
 - Collected information is then passed to the 'add_book()' function to add the book to the 'books_23094907.txt' file.
 '''
+
+
 book = add_book_information()
 add_book(book)
