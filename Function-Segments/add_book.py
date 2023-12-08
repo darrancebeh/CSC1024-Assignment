b834d14bc0f3f732654bce5ceb17766f4f2e95d6
LeeIsaac2004 <152567159+LeeIsaac2004@users.noreply.github.com>
2023-12-08 12:56:24 +0800

    Update add_book.py
    Fifth revision. Please let me know if the formatting error persists.

--- Function-Segments/add_book.py
+++ Function-Segments/add_book.py
@@ -1,25 +1,47 @@
+'''
+- The statement `from datetime import datetime` is used to directly import the `datetime` class from the `datetime` module.
+- Commonly used for handling dates and times.
+'''
 from datetime import datetime
 
-
+'''
+- The 'get_book_information()' function collects and validates information about a book from the user.
+- It prompts the user to enter the ISBN number, author's name, book's title, publisher's name, genre, publishing year, purchase date, and book's status.
+- Naming convention taken into consideration as people from different cultures have different naming conventions, such as umlauts and ligatures.
+- Also performs validation checks for the ISBN number to detect for existing matching books.
+'''
 def get_book_information():
     print("Please enter the following information:")
     while True:
         isbn = input("ISBN number: ")
-        if len(isbn) == 13:
-            break
-        else:
-            print("Error. ISBN number must be 13 digits long. Please try again.")
-    author = input("Author's name: ")
-    title = input("Book's title: ")
-    publisher = input("Publisher's name: ")
-    genre = input("Genre of the book: ")
-    while True:
-        published_year = input("Publishing year: ")
-        if published_year.isdigit() and int(published_year) > 0 and int(published_year) <= datetime.now().year:
+        if len(isbn) == 13 and isbn.isdigit():
             break
         else:
             print(
-                "Error. Publishing year must be a positive integer and not in the future. Please try again.")
+                "Error. ISBN number must be 13 digits long and contain only digits. Please try again.")
+    while True:
+        author = input("Author's name: ")
+        if all(word.isalpha() for word in author.split()):
+            break
+        else:
+            print(
+                "Error. Author's name must only contain alphabetical characters. Please try again.")
+    title = input("Book's title: ")
+    publisher = input("Publisher's name: ")
+    while True:
+        genre = input("Genre of the book: ")
+        if all(word.isalpha() for word in genre.split()):
+            break
+        else:
+            print(
+                "Error. Genre must only contain alphabetical characters. Please try again.")
+    while True:
+        published_year = input("Publishing year: ")
+        if len(published_year) == 4 and published_year.isdigit() and int(published_year) > 0 and int(published_year) <= datetime.now().year:
+            break
+        else:
+            print(
+                "Error. Publishing year must be a 4-digit positive integer and not in the future. Please try again.")
     while True:
         date_purchased_str = input("Date purchased in the format DD-MM-YYYY: ")
         try:
@@ -31,20 +53,29 @@ def get_book_information():
                     "Error. The purchase date cannot be in the future. Please try again.")
         except ValueError:
             print("Error. The date format is incorrect. Please try again.")
-    status = input("Book's status: ")
+    while True:
+        print("Book's status options: Wishlist, To-read, Reading, Completed")
+        status = input("Book's status: ")
+        if status in ['Wishlist', 'To-read', 'Reading', 'Completed']:
+            break
+        else:
+            print("Error. Invalid status. Please try again.")
 
     return isbn, author, title, publisher, genre, published_year, date_purchased_str, status
 
-
+'''
+- Opens books_23094907.txt and write the new book's information into it.
+'''
 def add_book(book):
     with open("books_23094907.txt", 'a') as f:
         isbn, author, title, publisher, genre, published_year, date_purchased_str, status = book
-        book_information = f"{isbn}|{author}|{title}|{publisher}|{
-            genre}|{published_year}|{date_purchased_str}|{status}"
+        book_information = f"{isbn}|{author}|{title}|{publisher}|{genre}|{published_year}|{date_purchased_str}|{status}"
         f.write('\n' + book_information)
     print("Book added successfully!")
 
-
-# Example usage:
+'''
+- Calls the 'get_book_information()' function to collect book information from the user.
+- Collected information is then passed to the 'add_book()' function to add the book to the 'books_23094907.txt' file.
+'''
 book = get_book_information()
 add_book(book)
