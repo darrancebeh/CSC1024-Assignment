8150f2e4377edf31b29a25e7014e13d2eed10d5c
4dummy <149559666+4dummy@users.noreply.github.com>
2023-12-09 16:57:12 +0800

    Delete 1 - test for display and sort.py

--- Function-Segments/1 - test for display and sort.py
+++
@@ -1,9 +0,0 @@
-from f1_display_books import display_books
-from aux6_sort_books import ask_sort_books
-
-display_books()
-ask_sort_books()
-
-# Notes:
-# If there are duplicate copies but different date purchased/status, it works but doesn't order them in any particular way.
-# If sorted by date of purchase, only sorts by the first digit in date. Probably better to not include the option?
